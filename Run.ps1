ffcc865358b1586679fa82983953b9394892722b
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2022-09-28 15:26:55 +0700

    Add Live Coding Page (#1)
    * Fix the Run.ps1 script remove the logging flag if not desired.
    
    * Fix the image link and cleanup formatting
    
    * Add top level page with minimal info regarding live coding.

--- Run.ps1
+++ Run.ps1
@@ -1,1 +1,1 @@
-dotnet run --project .\Source\blazor-university-com\blazor-university-com.csproj -- preview -l
+dotnet run --project .\Source\blazor-university-com\blazor-university-com.csproj -- preview
