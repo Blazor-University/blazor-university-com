82853eb7f511e88c565594189f85e3ab30fdca21
Peter Morris <mrpmorris@gmail.com>
2026-07-20 15:45:29 +0100

    Markdown fallback

--- source/blazor-university-com/Program.cs
+++ source/blazor-university-com/Program.cs
@@ -1,6 +1,8 @@
 ﻿namespace BlazorUniversityCom;
 using System.Threading.Tasks;
 using Statiq.App;
+using Statiq.Common;
+using Statiq.Core;
 using Statiq.Docs;
 using Statiq.Lunr;
 
@@ -12,5 +14,16 @@ internal class Program
       .Factory
       .CreateDocs(aArgumentArray)
       .AddSetting(CustomKeys.GenerateSearchIndex,true)
+      .AddPipeline(
+        "RawMarkdown",
+        new ReadFiles("**/*.md"),
+        new ExtractFrontMatter(new Statiq.Yaml.ParseYaml()),
+        // Content pages live under input/pages but are served from the site root
+        new SetDestination(
+          Config.FromDocument(doc =>
+            doc.Destination.Segments.Length > 0 && doc.Destination.Segments[0].ToString() == "pages"
+              ? new NormalizedPath(doc.Destination.FullPath["pages/".Length..])
+              : doc.Destination)),
+        new WriteFiles())
       .RunAsync();
 }
